39c50deed19b4efafe61210e9d337f31869baa92
elenolcar <Black_Star_freak@hotmail.com>
2021-03-18 19:31:08 +0100

    Co-authored-by: Alvaro Cortes <alvcorcas@users.noreply.github.com>

--- src/main/java/org/springframework/samples/petclinic/repository/BookingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/BookingRepository.java
@@ -1,13 +1,20 @@
+
 package org.springframework.samples.petclinic.repository;
 
-import org.springframework.samples.petclinic.model.Booking;
+import java.util.List;
+
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.model.Booking;
+import org.springframework.samples.petclinic.model.PetType;
 
 
-public class BookingRepository extends CrudRepository<Booking, Integer>{
-    
-    Booking findById(int id) throws DataAccessException;
+public interface BookingRepository extends Repository<Booking, Integer> {
+	
+	Booking findById(int id) throws DataAccessException;
 
+	
 	void save(Booking booking) throws DataAccessException;
 }
