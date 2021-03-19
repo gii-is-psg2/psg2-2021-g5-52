b70d4a552be16d85d5413dfee762198605539dc5
alvcorcas <alvarocortescasado@gmail.com>
2021-03-19 12:19:20 +0100

    BookingServices mtehods and querys

--- src/main/java/org/springframework/samples/petclinic/repository/BookingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/BookingRepository.java
@@ -1,20 +1,17 @@
-
 package org.springframework.samples.petclinic.repository;
 
 import java.util.List;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
-import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.samples.petclinic.model.Booking;
-import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.Owner;
 
 
-public interface BookingRepository extends Repository<Booking, Integer> {
-	
-	Booking findById(int id) throws DataAccessException;
+public interface BookingRepository extends CrudRepository<Booking, Integer>{
 
-	
-	void save(Booking booking) throws DataAccessException;
+	@Query("SELECT b FROM Booking b WHERE b.pet.owner = :owner")
+	List<Booking> getOwnersBookings(@Param("owner") Owner owner);
+   
 }
--- src/main/java/org/springframework/samples/petclinic/service/BookingService.java
+++ src/main/java/org/springframework/samples/petclinic/service/BookingService.java
@@ -1,5 +1,38 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Booking;
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.repository.BookingRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookingService {
-    
+
+	private final BookingRepository bookingRepository;
+
+	@Autowired
+	public BookingService(final BookingRepository bookingRepository) {
+		super();
+		this.bookingRepository = bookingRepository;
+	}
+	
+	public void saveBooking(final Booking booking) {
+		this.bookingRepository.save(booking);
+	}
+	
+	public void deleteBooking(final Booking booking) {
+		this.bookingRepository.delete(booking);
+	}
+	
+	public List<Booking> getOwnersBookings(final Owner owner){
+		return this.bookingRepository.getOwnersBookings(owner);
+	}
+	
+	
+	
+	
+	
 }
